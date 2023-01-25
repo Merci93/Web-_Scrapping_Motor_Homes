6fad6977e9948f60e9e030988662836b1b61ab0b
David Ugochukwu Asogwa <asogwadavidu@gmail.com>
2023-01-25 10:59:05 +0100

    py script

--- scrape.py
+++ scrape.py
@@ -1,115 +1,128 @@
-# Import modules
 import re
+import lxml
+import time
 import pandas as pd
-import requests as rq
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup as bs
 
-########################################################################################################################################################################################
 
-def get_page(page_url):
-    '''
-    A function to save a webpage as HTML file, parse and return parsed page.
+def scrape_data(url):
+    """
+    A script to scrape data from given url.
 
-    Args:
-        page_url: url of the page.
-    '''
-    response = rq.get(page_url)
-    parse_page = bs(response.text, 'lxml')
-
-    return parse_page
-
-
-def scrape_data(url, fuel_type):
-    '''
-     A function to scrape a web page: motor homes
-     
-     Args:
-         url: url for the page to scrape
-         fuel_type: Fuel type for the motor
-    '''
-      
-    # list to hold extracted data
-    df_list = [] 
+    Keyword arguments:
+    url: url link to the page.
+    """
     
-    # Iterate through all pages (20 pages)
-    for i in range(1, 21): 
-        page_url = url + '?page=' + str(i)
-        parse_page = get_page(page_url) # function call to get page
-        search_body = parse_page.find('div', {'id': 'content-search__body'}) # Get the section with container frames
-        container_frames = re.findall('id="pagination_container_\w+"', str(search_body)) # regular expression to find all item containers
-        
-        # loop through each container item and get required details
-        for i in range(1, len(container_frames) + 1, 1):
-            item_url_short = parse_page.find('div', {'id': 'pagination_container_' + str(i)}).find('div', {'class':'product-btns'}).find_all('a', {'clas':''})[1]['href']
-            item_url_full = 'https://rv.campingworld.com' + str(item_url_short) # url to item details
-            parse_item = get_page(item_url_full) # function call to get item page
-            tab_content = parse_item.find('div', {'class':'tab-content'}).find_all('div', {'class':'oneSpec clearfix'}) # Get tab container with specifications
-            specifications_1 = [spec.find('h4').text for spec in tab_content] # list containing specification title
-            specifications_2 = [spec.find('h5').text for spec in tab_content] # list containing specifications of each item
-            specifications = dict(zip(specifications_1, specifications_2)) # dictionary containing motor specifications
+    #driver = webdriver.Chrome()
+    #driver_path = 'Add the path to chrome driver here'
+    driver_path = 'C:/Users/David Ugochi Asogwa/Documents/Folders/GitHub/Web_Scrapping_Motor_Homes/chromedriver.exe'
+    driver = webdriver.Chrome(service = Service(driver_path))
+    
+    # Get URL
+    # Select RV class and fuel type: Diesel
+    driver.get(url)
+    rv_class = driver.find_element(By.XPATH, '//*[@id="rv_class"]/fieldset/div[1]').click()
+    time.sleep(1)
+    fuel_type = driver.find_element(By.XPATH, '//*[@id="classAFuel"]/div[3]/label').click()
 
-            # Get vehicle details if it meets gas type specification
-            # Exception handling to skip items with missing fuel type in their specifications
+    # List to hold extracted data.
+    df_list = []
+
+    # Loop through pages till end.
+    # Get current page source and container frames.
+    # loop through all item in the contaimer frames collecting required data.
+
+    while True:
+
+        time.sleep(1)
+        search_body = bs(driver.page_source, 'lxml')
+        container_frames = re.findall('id="pagination_container_\w+"', str(search_body))
+
+        for container in container_frames:
+            time.sleep(1)
+            vehicle_name = driver.find_element(By.XPATH, '//*[@' + str(container)
+                                               + ']/div/div[3]/div[1]/div[1]/a/span').text
+            status = driver.find_element(By.XPATH, '//*[@' + str(container)
+                                         + ']/div/div[3]/div[1]/div[1]/span').text.capitalize()
+            location = driver.find_element(By.XPATH, '//*[@' + str(container) + ']/div/div[3]/div[1]/div[2]/span[1]').text
+            stock_number = driver.find_element(By.XPATH, '//*[@'
+                                               + str(container)
+                                               + ']/div/div[3]/div[1]/div[2]/span[2]').text.split(' ')[2]
+            length = driver.find_element(By.XPATH, '//*[@' + str(container) + ']//div[@class="specs"]').text.split('\n')[1]
+            sleeps = driver.find_element(By.XPATH, '//*[@' + str(container)
+                                         + ']//div[@class="specs"][2]').text.split('\n')[1]
             try:
-                if fuel_type.casefold() == specifications['FUEL TYPE'].casefold():
-                    vehicle_name =  parse_item.find('div', {'class':'card__title'}).find('h1', {'itemprop': 'name'}).text.split(' ', 1)[1].replace(' ', '', 31)
-                    stock_number = parse_item.find('div', {'class':'stock-num-prod-details'}).text.split(' ')[2]
-                    status = parse_item.find('div', {'class':'product-card-line'}).find('h1', {'id': '#used-or-new'}).text
-                    location = parse_item.find('span', {'class':'stock-results'}).find('b').text + ', ' + list(parse_item.find('span', {'class':'stock-results'}).stripped_strings)[1]
-                    sales_price = parse_item.find('span', {'class':'price-info low-price'}).text[1:].replace(',', '')
-                    
-                    # Exception handling for cases of missing specification item
-                    try:
-                        sleeps = specifications['SLEEPS']
-                    except:
-                        sleeps = 'N/A'
-                    try:
-                        length = specifications['LENGTH'][:5]
-                    except:
-                        length = 'N/A'
-                    try:
-                        if int(sales_price) > 300000:
-                            horse_power = specifications['HORSEPOWER']
-                        else:
-                            horse_power = 'N/A'
-                    except:
-                        horse_power = 'N/A'
+                sales_price = driver.find_element(By.XPATH, '//*[@'
+                                                  + str(container) 
+                                                  + ']//span[@class="price-info low-price "]').text[1:].replace(',', '')
+            except NoSuchElementException:
+                sales_price = driver.find_element(By.XPATH, '//*[@'
+                                                  + str(container)
+                                                  + ']//span[@class="price-info low-price"]').text[1:].replace(',', '')
 
-                    #append extracted data to list of dictionaries
-                    df_list.append({'vehicle_name': vehicle_name,
-                                    'stock_number': stock_number,
-                                    'status': status,
-                                    'location': location,
-                                    'fuel_type': fuel_type,
-                                    'sleeps': int(sleeps),
-                                    'length (inches)': float(length),
-                                    'sale_price ($)': float(sales_price),
-                                    'horse_power': horse_power})
-            except:
-                continue
-    
-    # transform extracted data into a data frame
+            try:
+                if int(sales_price) > 300000:
+                    get_details = driver.find_element(By.XPATH, '//*[@'+str(container)+']/div/div[3]/div[4]/a[2]').click()
+                    time.sleep(2)
+                    get_page_source = bs(driver.page_source, 'lxml')
+                    tab_content = get_page_source.find('div', {'class':'tab-content'}).find_all('div',
+                                                                                         {'class':'oneSpec clearfix'})
+                    specifications_1 = [spec.find('h4').text for spec in tab_content]
+                    specifications_2 = [spec.find('h5').text for spec in tab_content]
+                    specifications = dict(zip(specifications_1, specifications_2))
+                    horse_power = specifications['HORSEPOWER']
+                    back_to_results = driver.find_element(By.ID, 'back-link').get_attribute('href')
+                    driver.get(back_to_results)
+
+                else:
+                    horse_power = 'N/A'
+
+            except KeyError:
+                horse_power = 'N/A'
+
+                # Return to search results.
+                time.sleep(1)
+                back_to_results = driver.find_element(By.ID, 'back-link').get_attribute('href')
+                driver.get(back_to_results)
+
+            # Append extracted data to list of dictionaries.
+            df_list.append({'vehicle_name': vehicle_name,
+                            'stock_number': stock_number,
+                            'status': status,
+                            'location': location,
+                            'sleeps': sleeps,
+                            'length': length,
+                            'sales_price (USD)': int(sales_price),
+                            'horse_power': horse_power})
+
+        # Go to next page.
+        try:
+            next_page = driver.find_element(By.XPATH, '//*[@id="page_next"]').click()
+        except NoSuchElementException:
+            break         # Break at the end of the page.
+
+    # Close driver.
+    driver.close()
+
+    # Transform extracted data into a data frame.
     df = pd.DataFrame(df_list, columns = ['vehicle_name', 'stock_number', 'status', 'location','fuel_type', 'sleeps',
-                                          'length (inches)', 'sale_price ($)', 'horse_power'])
+                                          'length', 'sales_price (USD)', 'horse_power'])
+
+    #df.to_csv('RV_MotorHomes_with_possible_duplicates.csv', index = False)
     
-    # drop duplicates from the data set if exists
+    # Drop duplicates from the data set.
     df.drop_duplicates(inplace = True)
-    
-    # save dataframe as csv
-    df.to_csv(fuel_type + '_RV_Motorhomes.csv', index = False) 
-    
-    return df
+
+    # Save dataframe as CSV.
+    df.to_csv('RV_MotorHomes.csv', index = False)
 
 
-#######################################################################################################################################################################################
-
-# inputs
-
-# fuel_type = 'Diesel'
-# fuel_type = 'Gas'
-
-url = 'https://rv.campingworld.com/rvclass/motorhome-rvs'
-fuel_type = str(input('Enter fuel type: '))
-
-# Run program
-scrape_data(url, fuel_type)
+if __name__ == '__main__':
+    #url = 'https://rv.campingworld.com/rvclass/motorhome-rvs'
+    url = str(input('Enter URL: '))
+    scrape_data(url)
